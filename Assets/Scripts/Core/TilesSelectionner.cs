85a9f7391b212850e101e5a71121a87c35f26c8a
Leithianîn <m.lukas@rubika-edu.com>
2021-03-02 17:45:32 +0100

    Tile selection wip

---
+++ Assets/Scripts/Core/TilesSelectionner.cs
@@ -0,0 +1,43 @@
+﻿using Flux.Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TilesSelectionner : MonoBehaviour
+{
+    private Map map;
+
+    private InputAction clickAction;
+    private InputAction mousePosAction;
+
+    private Vector2 mousePosition;
+
+    private void OnEnable()
+    {
+        clickAction.performed += OnClick;
+        mousePosAction.performed += OnMouseMove;
+    }
+
+    private void OnDisable()
+    {
+        clickAction.performed -= OnClick;
+        mousePosAction.performed -= OnMouseMove;
+    }
+
+    private void Start()
+    {
+        map = Repository.Get<Map>(References.Map);
+    }
+
+    void OnClick(InputAction.CallbackContext context)
+    {
+        var mouseToTile = map.Tilemap.WorldToCell(mousePosition);
+        Debug.Log(mouseToTile);
+    }
+
+    void OnMouseMove(InputAction.CallbackContext context)
+    {
+        mousePosition = context.ReadValue<Vector2Int>();
+    }
+}

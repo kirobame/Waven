a9313051d2a19eb98dd5481136edd0e1c9793517
Leithianîn <m.lukas@rubika-edu.com>
2021-03-03 16:11:30 +0100

    Tooltip can't go outside screen space

--- Assets/Scripts/UI/PointerHandler.cs
+++ Assets/Scripts/UI/PointerHandler.cs
@@ -9,14 +9,14 @@ using UnityEngine.InputSystem;
 public class PointerHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private SpellTooltip tooltip;
+    private RectTransform rectTooltip;
     private UISpellData spellData;
 
-    private Vector2 mousePosition;
-
-    private bool pointerOnUI;
+    private bool pointerOnUI;    
 
     private void Start()
     {
+        rectTooltip = Repository.Get<RectTransform>(References.TooltipBackground);
         tooltip = Repository.Get<RectTransform>(References.TooltipBackground).GetComponent<SpellTooltip>();
         spellData = gameObject.GetComponent<UISpellData>();
     }
@@ -25,7 +25,16 @@ public class PointerHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     {
         if (!pointerOnUI) return;
 
-        tooltip.transform.position = Mouse.current.position.ReadValue();
+        var tooltipPosition = Mouse.current.position.ReadValue().x + rectTooltip.sizeDelta.x;
+        if(tooltipPosition > (Screen.width - tooltip.screenMargin))
+        {
+            var offset = tooltipPosition - (Screen.width - tooltip.screenMargin);
+            tooltip.transform.position = new Vector2((Mouse.current.position.ReadValue().x - offset), Mouse.current.position.ReadValue().y);
+        }
+        else
+        {
+            tooltip.transform.position = Mouse.current.position.ReadValue();
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)

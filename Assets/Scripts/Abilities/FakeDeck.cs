14d5aeddd258e18c3c4d231212bb03b3a3b906c6
Leithianîn <m.lukas@rubika-edu.com>
2021-03-03 12:24:09 +0100

    Spells UI display & Tooltip wip

---
+++ Assets/Scripts/Abilities/FakeDeck.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeDeck : MonoBehaviour
+{
+    public List<FakeSpell> deck = new List<FakeSpell>();
+}
---
+++ Assets/Scripts/Abilities/FakeSpell.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewFakeSpell", menuName = "Waven/FakeSpell")]
+public class FakeSpell : ScriptableObject, ICastable
+{
+    public string Name => title;
+    [SerializeField] private string title;
+
+    public string Description => description;
+    [SerializeField, TextArea] private string description;
+
+    public Sprite Thumbnail => thumbnail;
+    [SerializeField] private Sprite thumbnail;
+}

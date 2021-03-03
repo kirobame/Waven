1b027243a57b9f61a707c0b3fc2d0990ccd4f358
Arthur Herbreteau <a.herbreteau@rubika-edu.com>
2021-03-03 21:43:27 +0100

    Add interfaces

---
+++ Assets/Scripts/Interfaces/IDammageable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDammageable
+{
+    int lifePoints { get; set; }
+    int TakeDamage(int damage);
+}

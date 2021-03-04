285a2dcac2156d35028208a3cc8415f5cf3a83d4
Leithianîn <m.lukas@rubika-edu.com>
2021-03-04 10:23:15 +0100

    Created push effect & scriptable

---
+++ Assets/Scripts/Terrain/Push.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[Serializable]
+public class Push : Effect
+{
+    [SerializeField] int force;
+
+    protected override void ApplyTo(IEnumerable<Tile> tiles)
+    {
+        var targets = tiles.SelectMany(tile => tile.Entities).Where(entity => entity is Player player && player.TeamId != Player.Active.TeamId);
+
+        foreach(var target in targets) Debug.Log($"Pushing player {force} tiles away");
+    }
+}

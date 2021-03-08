8ac4728848b00ad72a19884efd8b152cc91df099
Leithianîn <m.lukas@rubika-edu.com>
2021-03-08 15:07:21 +0100

    Fix : EntityInfo now displaying only actual life value

--- Assets/Scripts/Interface/Infos/EntityInfo.cs
+++ Assets/Scripts/Interface/Infos/EntityInfo.cs
@@ -65,7 +65,7 @@ public class EntityInfo : MonoBehaviour
         var ratio = health.Ratio;
 
         slider.value = ratio;
-        infos[index].Assign(stats.Values[StatType.Health], $"{health.actualValue} / {health.maxValue}");
+        infos[index].Assign(stats.Values[StatType.Health], $"{health.actualValue}");
         
         index++;
 

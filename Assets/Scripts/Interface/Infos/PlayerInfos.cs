277d671e568579466f97fbb1968c5a1880a99c72
Leithianîn <m.lukas@rubika-edu.com>
2021-03-08 17:44:14 +0100

    Player profile selection wip

---
+++ Assets/Scripts/Interface/Infos/PlayerInfos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfos : MonoBehaviour
+{
+    [SerializeField] private string playerTag;
+
+    private void Update()
+    {
+        /*if (Player.Active.GetComponent<Tag>().tag == playerTag)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }*/
+    }
+}

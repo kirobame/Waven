1484449caac6e682a46c8099c2286507bb6a5c8b
Margaux LUKAS <m.lukas@rubika-edu.com>
2021-03-11 01:12:00 +0100

    Tooltip anchors

--- Assets/Scripts/Interface/Tooltip/PointerHandler.cs
+++ Assets/Scripts/Interface/Tooltip/PointerHandler.cs
@@ -14,13 +14,15 @@ public class PointerHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitH
 
     public SpellBase Spell => relay.Value;
     [SerializeField] private SpellHolder relay;
+
+    [SerializeField] private RectTransform tooltipPos;
     
     public void OnPointerEnter(PointerEventData eventData)
     {
         onEnter?.Invoke(this);
         
         var message = $"<size=100%><b>{Spell.Title}</size></b>\n<size=65%>{Spell.Description}</size>";
-        Events.ZipCall(InterfaceEvent.OnTooltipUsed, message);
+        Events.ZipCall(InterfaceEvent.OnTooltipUsed, message, tooltipPos, 2);
     }
 
     public void OnPointerExit(PointerEventData eventData)

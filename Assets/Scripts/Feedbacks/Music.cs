36ff9ceb61142be2704bbbac8385b84f0c44aea2
Robert <r.biaulet@rubika-edu.com>
2021-03-14 14:19:50 +0100

    Integration

---
+++ Assets/Scripts/Feedbacks/Music.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private static Music instance;
+    
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void OnDestroy()
+    {
+        if (instance != this) return;
+        instance = null;
+    }
+}
